f1deeafeabb25bac2a368c6bd850ce2b6a1a96b7
Majid Taherkhani <majidtaherkhani555@gmail.com>
2025-12-17 18:33:59 -0500

    fix backend url

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const apiClient = axios.create({
-  baseURL: 'https://etf-service-th2v.onrender.com/etf/analyze', // Python Backend URL
+  baseURL: 'https://etf-service-th2v.onrender.com', // Python Backend URL
   headers: {
     'Content-Type': 'application/json',
   },
